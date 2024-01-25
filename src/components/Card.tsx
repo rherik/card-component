110abf385c57dc76375c708edf17dbfb38ee9559
rherik <kurtzgk@gmail.com>
2024-01-24 21:23:44 -0300

    feat: add dark mode button

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,30 +1,21 @@
-import React, { useEffect } from 'react';
+import { FaRegSun as Sun } from "react-icons/fa";
+import { IoIosMoon as Moon } from "react-icons/io";
 import { CardInterface } from "../types";
 import Badge from "./Badge";
 import Button from "./Button";
 import styles from './Card.module.css';
 
 const Card = ({ body, btn, title, badge, image, indicator, subtitle, btn2 }: CardInterface) => {
-  type Themes = "light" | "dark";
-  const themes2: Themes[] = ["light", "dark"];
-
-  const changeTheme = (theme: Themes) => {
-    document.documentElement.setAttribute("data-theme", theme);
+  const setDarkMode = () => {
+    document.documentElement.setAttribute("data-theme", 'dark');
   };
-
-  const handleThemeClick = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const theme = e.target.id as Themes;
-    changeTheme(theme);
+  const setLightMode = () => {
+    document.documentElement.setAttribute("data-theme", 'light'); 
   };
-
-  useEffect(() => {
-    const themeToggles = document.querySelectorAll<HTMLInputElement>('[name="themeSwitcher"]');
-    themeToggles.forEach((t) => t.addEventListener("change", handleThemeClick));
-
-    return () => {
-      themeToggles.forEach((t) => t.removeEventListener("change", handleThemeClick));
-    };
-  }, []); // Adiciona um ouvinte de evento no montante e remove no desmontante
+  const toggleTheme = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked) setDarkMode();
+    else setLightMode();
+  }
 
   return (
     <article className={`stack-lg ${styles.card}`}>
@@ -40,14 +31,21 @@ const Card = ({ body, btn, title, badge, image, indicator, subtitle, btn2 }: Car
         {btn && <Button text={btn.text} type={btn.type} filled={btn.filled} referencia={btn.referencia} icon={btn.icon} />}
         {btn2 && <Button text={btn2.text} type={btn2.type} filled={btn2.filled} referencia={btn2.referencia} icon={btn2.icon} />}
       </div>
-      <div className={styles.theme_switcher}>
-        {themes2.map((t) => (
-          <label key={t}>
-            {t}
-            <input type="radio" name="themeSwitcher" id={t} onChange={handleThemeClick} />
-          </label>
-        ))}
-      </div>
+      <div className={styles.dark_mode}>
+            <label className={styles.dark_mode_label}>
+            <input
+                className={styles.dark_mode_input}
+                type='checkbox'
+                id='darkmode-toggle'
+                onChange={toggleTheme}
+            />
+              <span className={styles.slider}>
+              <Sun className={styles.sun}/>
+              <Moon className={styles.moon}/>
+              </span>
+            </label>
+        </div>
+
     </article>
   );
 };
